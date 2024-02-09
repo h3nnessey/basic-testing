76ef0ca16b1da8ef795c48df5f8507b10a832f5a
Vitaly <vitaliy.zayakin@gmail.com>
2024-02-09 16:49:33 +0500

    feat: solve 08-snapshot-testing

--- src/08-snapshot-testing/index.test.ts
+++ src/08-snapshot-testing/index.test.ts
@@ -18,6 +18,7 @@ describe('generateLinkedList', () => {
   });
 
   test('should generate linked list from values 2', () => {
+    // building a linked list even on the base of these few elements is time-consuming and extremely awkward
     const elements = ['d', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o'];
 
     expect(generateLinkedList(elements)).toMatchSnapshot();
