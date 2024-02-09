bae7653ad148a311239b9d19339c045bbde87cbe
Vitaly <vitaliy.zayakin@gmail.com>
2024-02-09 19:06:24 +0500

    feat: solve 07-mocking-lib-api

--- src/07-mocking-lib-api/index.test.ts
+++ src/07-mocking-lib-api/index.test.ts
@@ -1,17 +1,61 @@
-// Uncomment the code below and write your tests
-/* import axios from 'axios';
-import { throttledGetDataFromApi } from './index'; */
+import axios from 'axios';
+import lodash from 'lodash';
+import { throttledGetDataFromApi } from './index';
 
 describe('throttledGetDataFromApi', () => {
+  const BASE_URL = 'https://jsonplaceholder.typicode.com';
+  const RELATIVE_PATH = 'posts/1';
+  const RESPONSE = {
+    data: {
+      userId: 1,
+      id: 1,
+      title: 'lorem ipsum',
+      body: 'lorem ipsum',
+    },
+  };
+
+  beforeAll(() => {
+    jest.useFakeTimers();
+  });
+
+  afterAll(() => {
+    jest.useRealTimers();
+  });
+
+  beforeEach(() => {
+    jest.spyOn(axios, 'create').mockReturnThis();
+    jest.spyOn(axios, 'get').mockResolvedValue(RESPONSE);
+    jest.spyOn(lodash, 'throttle').mockImplementation((fn) => fn());
+  });
+
+  afterEach(() => {
+    jest.restoreAllMocks();
+  });
+
   test('should create instance with provided base url', async () => {
-    // Write your test here
+    const createSpy = jest.spyOn(axios, 'create');
+
+    await throttledGetDataFromApi(RELATIVE_PATH);
+
+    jest.runAllTimers();
+
+    expect(createSpy).toHaveBeenCalledWith({ baseURL: BASE_URL });
   });
 
   test('should perform request to correct provided url', async () => {
-    // Write your test here
+    const getSpy = jest.spyOn(axios, 'get');
+
+    await throttledGetDataFromApi(RELATIVE_PATH);
+
+    jest.runAllTimers();
+    expect(getSpy).toHaveBeenCalledWith(RELATIVE_PATH);
   });
 
   test('should return response data', async () => {
-    // Write your test here
+    const response = await throttledGetDataFromApi(RELATIVE_PATH);
+
+    jest.runAllTimers();
+
+    expect(response).toStrictEqual(RESPONSE.data);
   });
 });
